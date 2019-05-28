7b1b9cf2c5139f345fe0ff779d49eefc6368a551
Alejandro Vivas <ajvivas86@gmail.com>
2019-05-28 13:41:39 -0400

    Removed unnecesary dependencies

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,1 @@
 import '../scss/app.scss'
-import hljs from 'highlight.js'
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
-  });
-});
